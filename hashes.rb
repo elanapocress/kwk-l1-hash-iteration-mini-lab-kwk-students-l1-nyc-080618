babc28bb8945ed74359b5a357a97d72bd22c78bb
elanapocress <pocresse@gmail.com>
2018-08-09 14:22:15 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,16 @@
+# place_array = ["Sydney", "Athens", "Beijing", "London"]
+# year_array = [200, 2004, 2008, 2012]
+
+# summer_olympics_hash = {}
+#   counter = 0 
+#   place_array.each do |place|
+#     summer_olympics_hash [place] = place [counter]
+#     counter+=1 
+#   end
+  
+#   puts summer_olympics_hash
+
+
+
+  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
+  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
